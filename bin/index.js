a083cfdcd5c8f53b31638f7e6dd9f110919ee75c
lixuelong <lixuelong@aixuedai.com>
2016-08-29 17:53:01 +0800

    test  commander

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,14 @@
 var  program = require('commander');
+var  initProject = require('../lib/initProject.js');
 
 program.version('0.0.1')
     .option("-f,--full",'show a full program')
     .option("-F,--fuck",'what a fuck man')
+    .parse(process.argv);
+
 if(program.full){
     console.log("this is a full....");
+    initProject.full();
 }
 if(program.fuck){
     console.log("this is a fuck....");
