1167c8ea783ca4f1a8d6566fd0b4c49dcfecef5b
lixuelong <lixuelong@aixuedai.com>
2016-08-29 17:14:11 +0800

    test

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,11 @@
-/**
- * Created by neo on 2016/8/17.
- */
+var  program = require('commander');
+
+program.version('0.0.1')
+    .option("-f,--full",'show a full program')
+    .option("-F,--fuck",'what a fuck man')
+if(program.full){
+    console.log("this is a full....");
+}
+if(program.fuck){
+    console.log("this is a fuck....");
+}
